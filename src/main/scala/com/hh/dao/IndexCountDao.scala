c7bfce1bf395229b964f64b6807ab7f7d1c24ceb
git-lyn <1415883718@qq.com>
2019-10-25 16:38:51 +0800

    实时统计pv、uv

--- src/main/scala/com/hh/dao/IndexCountDao.scala
+++ src/main/scala/com/hh/dao/IndexCountDao.scala
@@ -69,8 +69,72 @@ object IndexCountDao {
     // 使用完成后将对象返回给对象池
     mySqlPool.returnObject(client)
   }
+
+
+  // 向mysql插入数据
+  def insertBatch(indexClickCounts: Array[IndexClickCount]) {
+    // 获取对象池单例对象
+    val mySqlPool = CreateMySqlPool()
+    // 从对象池中提取对象
+    val client = mySqlPool.borrowObject()
+
+
+    // 区分开来哪些是要插入的，哪些是要更新的
+    val insertIndexClickCounts = ArrayBuffer[IndexClickCount]()
+    val updateIndexClickCounts = ArrayBuffer[IndexClickCount]()
+
+    val selectSQL = "SELECT count(*) " +
+      "FROM index_gid_click_count " +
+      "WHERE date=? " +
+      "AND position=? " +
+      "AND userid=?"
+
+//    for (indexClickCount <- indexClickCounts) {
+//
+//      val params = Array[Any](indexClickCount.date, indexClickCount.position, indexClickCount.userid)
+//      // 通过查询结果判断当前项时待插入还是待更新
+//      client.executeQuery(selectSQL, params, new QueryCallback {
+//        override def process(rs: ResultSet): Unit = {
+//          if (rs.next() && rs.getInt(1) > 0) {
+//            updateIndexClickCounts += indexClickCount
+//          } else {
+//            insertIndexClickCounts += indexClickCount
+//          }
+//        }
+//      })
+//    }
+
+    // 对于需要插入的数据，执行批量插入操作
+    val insertSQL = "INSERT INTO index_gid_click_count VALUES(?,?,?,?)"
+
+    val insertParamsList: ArrayBuffer[Array[Any]] = ArrayBuffer[Array[Any]]()
+
+    for (indexClickCount <- indexClickCounts) {
+      insertParamsList += Array[Any](indexClickCount.date, indexClickCount.position, indexClickCount.userid, indexClickCount.clickCount)
+    }
+
+//    client.executeBatch(insertSQL, insertParamsList.toArray)
+    client.executeBatch(insertSQL, insertParamsList.toArray)
+
+    // 对于需要更新的数据，执行批量更新操作
+    // 此处的UPDATE是进行覆盖
+//    val updateSQL = "UPDATE index_gid_click_count SET clickCount=? " +
+//      "WHERE date=? " +
+//      "AND position=? " +
+//      "AND userid=?"
+//
+//    val updateParamsList: ArrayBuffer[Array[Any]] = ArrayBuffer[Array[Any]]()
+//
+//    for (indexClickCount <- updateIndexClickCounts) {
+//      updateParamsList += Array[Any](indexClickCount.clickCount, indexClickCount.date, indexClickCount.position, indexClickCount.userid)
+//    }
+//
+//    client.executeBatch(updateSQL, updateParamsList.toArray)
+
+    // 使用完成后将对象返回给对象池
+    mySqlPool.returnObject(client)
+  }
+
 }
 
-object aa{
 
-}
